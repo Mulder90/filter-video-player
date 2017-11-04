f322a40a20a787ce2945a16a7864f2a22b4dfff2
Lorenzo Cinque <lore.cinque@gmail.com>
2017-11-04 09:15:25 +0100

    First commit

---
+++ src/index.js
@@ -0,0 +1,75 @@
+function insertAfter(referenceNode, newNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+
+class CanvasPlayer {
+  constructor(target, filter) {
+    this.video = document.getElementById(target);
+    this.initFrameBuffer();
+    this.initCanvas();
+    this.setSize();
+
+    this.filter = filter;
+
+    this.video.addEventListener('play', () => {
+      // this.video.style.display = 'none';
+      this.render();
+    });
+  }
+
+  initFrameBuffer() {
+    this.framebuffer = document.createElement('canvas');
+    this.framebuffer.style.display = 'none';
+    this.framebufferCtx = this.framebuffer.getContext('2d');
+    insertAfter(this.video, this.framebuffer);
+  }
+
+  initCanvas() {
+    this.canvas = document.createElement('canvas');
+    this.canvasCtx = this.canvas.getContext('2d');
+    insertAfter(this.framebuffer, this.canvas);
+  }
+
+  setSize() {
+    this.width = this.video.clientWidth;
+    this.height = this.video.clientHeight;
+    this.canvas.setAttribute('width', this.width);
+    this.canvas.setAttribute('height', this.height);
+    this.framebuffer.setAttribute('width', this.width);
+    this.framebuffer.setAttribute('height', this.height);
+  }
+
+  render() {
+    this.renderFrame();
+    window.requestAnimationFrame(() => {
+      this.render()
+    });
+  }
+
+  renderFrame() {
+    const data = this.getData();
+    this.transform(data);
+    this.draw(data);
+  }
+
+  getData() {
+    this.framebufferCtx.drawImage(
+      this.video,
+      0, 0,
+      this.video.videoWidth, this.video.videoHeight,
+      0, 0,
+      this.width, this.height
+    );
+    return this.framebufferCtx.getImageData(
+      0, 0, this.width, this.height
+    );
+  }
+
+  transform(data) {
+    this.filter(data);
+  }
+
+  draw(data) {
+    this.canvasCtx.putImageData(data, 0, 0);
+  }
+}
